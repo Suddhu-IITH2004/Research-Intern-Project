cc1b0b69da21db115eb21f08138a9adf5c45f91a
Suddhodhan <suddhu040804@gmail.com>
2023-06-23 19:09:12 +0530

    Fourth

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Research-Intern-Project/",
+  base:"/Research-Intern-Project",
   plugins: [react()],
 })
